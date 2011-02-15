cb85e36d91e96dee9958f7498b6a8ff9717f254a
Jason Frentsos <jason@wfme.net>
2011-02-15 11:22:57 -0500

    Updated the location of the Jonah/Caravan panel

--- content/javascript/jquery.home.js
+++ content/javascript/jquery.home.js
@@ -79,7 +79,7 @@
             tabs: true,
             tabsid: 'tabs-wrap',
             tabids: [ 'billboards', 'features', 'comments' ],
-            tabindexes: [ 0, 14, 18 ]            
+            tabindexes: [ 0, 15, 19 ]            
           });
         }
       });
