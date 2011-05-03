56715d6668a4164df5c5df2294cf4426a49a6593
Bill Burton <bburton@mail.com>
2011-05-03 09:13:50 -0400

    Update m3u.php to add Content-Disposition header with .m3u name.

--- content/m3u.php
+++ content/m3u.php
@@ -4,7 +4,9 @@ $dbg = $_GET['debug'];
 if (isset($url)) {
     $content_type = (isset($dbg) && $dbg == 'true') ? 'text/plain' : 'audio/x-mpegurl';
     $mp3_url = preg_replace('/^\//', "http://www.ebiblefellowship.com/", $url);
+    $m3u_file = preg_replace('/\.mp3$/', '.m3u', basename($url));
     header('Content-Type: ' . $content_type);
+    header('Content-Disposition: filename=' . $m3u_file);
     header('Content-Length: ' . strlen($mp3_url)+1);
     print $mp3_url . "\n";
 }
