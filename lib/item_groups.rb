d0d1753fe24cd4f9b0537c707f793b1ebbcc0697
Bill Burton <bburton@mail.com>
2011-06-13 08:04:47 -0400

    Update to new navigation feature to fix sorting.
    - lib/item_groups.rb - Added sort functionality here as it's critical
      to the correct operation of the navigation.
    
    - Rules - Added missing ".each" to sort item group; calls the sort
      functionality moved to the ItemGroups class.

--- lib/item_groups.rb
+++ lib/item_groups.rb
@@ -20,6 +20,8 @@
 #   end
 #
 # TODO: consider subclassing Hash instead of using delegation
+require 'time'
+
 class ItemGroups
 
   def initialize
@@ -55,5 +57,26 @@ class ItemGroups
 	@item_groups.size
   end
   alias :length :size
+
+  # Sort specified group by the :created_at date in ascending order
+  def sort_by_created_at(name)
+	return nil unless @item_groups.has_key?(name)
+	@item_groups[name].sort_by! do |a|
+	  time = a[:created_at]
+	  time.is_a?(String) ? Time.parse(time) : time
+	end
+	@item_groups[name]
+  end
+
+  def to_s
+	result = ''
+	@item_groups.each do |name, group|
+	  result << "Item group: #{name}:\n"
+	  group.each do |item|
+	    result << "  (#{item[:created_at]} - #{item.identifier})\n"
+	  end
+	end
+	result
+  end
 
 end
